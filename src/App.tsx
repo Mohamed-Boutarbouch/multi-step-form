08db5c0fdea72622572c7fe3ccbc2098174aac6c
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2023-02-10 18:53:25 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-primary-strawberry-red text-3xl font-bold underline">Hello</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      display: ['Ubuntu', 'monospace'],
+    },
+    extend: {
+      colors: {
+        primary: {
+          marine: {
+            blue: '#02295a',
+          },
+          purplish: {
+            blue: '#473dff',
+          },
+          pastel: {
+            blue: '#adbeff',
+          },
+          light: {
+            blue: '#bfe2fd',
+          },
+          strawberry: {
+            red: '#ed3548',
+          },
+        },
+        neutral: {
+          cool: {
+            gray: '#9699ab',
+          },
+          light: {
+            gray: '#d6d9e6',
+          },
+          magnolia: {
+            DEFAULT: '#f0f6ff',
+          },
+          alabaster: {
+            DEFAULT: '#fafbff',
+          },
+          white: {
+            DEFAULT: '#ffffff',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
